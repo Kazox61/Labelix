166b2d8178efc366a3aa5f7d40068d3005cd213c
Kazox6 <pawelka00florian@gmail.com>
2023-10-16 14:24:06 +0200

    update build settings

--- src/components/content/labelEditor.js
+++ src/components/content/labelEditor.js
@@ -186,6 +186,7 @@ export class LabelEditor extends ContentBase {
             const height = Math.abs(screenEY - startY);
             
             this.ctx.strokeStyle = this.selectedLabelClass.color;
+            this.ctx.lineWidth = 2;
             this.ctx.strokeRect(leftX, leftY, width, height);
         }
     }
@@ -279,6 +280,7 @@ export class LabelEditor extends ContentBase {
             const [screenX1, screenY1] = this.worldToScreen(x1, y1);
             const [screenX2, screenY2] = this.worldToScreen(x2, y2);
             this.ctx.strokeStyle = this.labelClasses[labelClassIndex].color;
+            this.ctx.lineWidth = 2;
             this.ctx.strokeRect(screenX1, screenY1, screenX2-screenX1, screenY2-screenY1);
         });
     }
